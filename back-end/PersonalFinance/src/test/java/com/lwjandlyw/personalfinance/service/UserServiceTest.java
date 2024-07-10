ef0f79149c0bd193879ff07a1f90d41108d5dc2e
Squareroot2-Bit <Squareroot_2@outlook.com>
2024-07-11 01:03:06 +0800

    用户注册登录功能测试

--- back-end/PersonalFinance/src/test/java/com/lwjandlyw/personalfinance/service/UserServiceTest.java
+++ back-end/PersonalFinance/src/test/java/com/lwjandlyw/personalfinance/service/UserServiceTest.java
@@ -1,11 +1,14 @@
 package com.lwjandlyw.personalfinance.service;
 
 import com.lwjandlyw.personalfinance.body.UserBody;
+import com.lwjandlyw.personalfinance.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @ClassName UserServiceTest
@@ -19,11 +22,37 @@ class UserServiceTest {
 
     @Autowired
     UserService service;
+
     @Test
     @Transactional
-    void registrationTest() {
-        UserBody userBody = new UserBody("1234567", "123456789");
+        //事务会自动回滚，数据不会插入到数据库中
+    void registration() {
+        //测试注册新用户
+        UserBody userBody = new UserBody("TangShiping", "000000");
         int registration = service.registration(userBody);
         assert registration == 1;
+        List<User> users = service.userMapper.selectUserByUsername("TangShiping");
+        assert users.size() == 1;
+        //测试重复注册用户
+        int registrationAgain = service.registration(userBody);
+        assert registrationAgain == -1;
+    }
+
+
+    @Test
+    @Transactional
+    void login() {
+        //测试登录成功
+        UserBody userBody0 = new UserBody("LiuWenjing", "000000");
+        int login0 = service.login(userBody0);
+        assert login0 == 1;
+        //测试用户不存在
+        UserBody userBody1 = new UserBody("TangShiping", "000000");
+        int login1 = service.login(userBody1);
+        assert login1 == -1;
+        //测试密码错误
+        UserBody userBody2 = new UserBody("LiuWenjing", "114514");
+        int login2 = service.login(userBody2);
+        assert login2 == -2;
     }
 }
