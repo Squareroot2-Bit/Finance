96f562dac9f6631f970fcfc8c5385d13ce49b5d2
https://se.bitcs.net.cn/1120212497 <1120212497@bit.edu.cn>
2024-07-11 22:28:40 +0800

    增加代码易读性并实现单元测试

--- front-end/src/views/RecordView.vue
+++ front-end/src/views/RecordView.vue
@@ -31,125 +31,18 @@
   </div>
 
   <div class="add_record">
-    <el-form
-    ref="recordFormRef"
-    :model="recordForm"
-    label-position="top"
-    class="demo-ruleForm"
-    :rules="rules"
-    status-icon
-  >
-  <h2>新增记录</h2>
-    <el-form-item label="日期" prop="date">
-      <el-date-picker v-model="recordForm.date" :disabled-date="disabledDate" type="date" placeholder="选择日期" style="width: 100%;" @change="formatUploadDate" />
-
-    </el-form-item>
-   <el-form-item label="金额" prop="money" >
-      <el-input-number v-model="recordForm.money" style="width: 100%;"  placeholder="请输入数字" :precision="2" :step="0.1" :max="100000000" :min="-100000000" />
-    </el-form-item>
-    <el-form-item label="分类" prop="tag" >
-      <el-select  v-model="recordForm.tag" placeholder="请选择分类" style="width: 100%;">
-        <el-option v-for="(tag,index) in RecordTag" :key="tag" :label="tag" :value="index" :disabled="index==0" ></el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item label="备注" prop="remark">
-      <el-input  v-model="recordForm.remark" type="textarea" placeholder="请输入备注" style="width: 100%;" maxlength="100"
-    show-word-limit></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-button type="primary" style="width: 100%; margin-top: 20px;" @click="submitForm(recordFormRef)">提交</el-button>
-    </el-form-item>
-
-  </el-form>
+    
       
 
-      
+      <UploadRecordView />
   </div>
   </div>
 </template>
 
 
 <script setup lang="ts">
-import { reactive, ref} from 'vue'
-import { ElMessage, type FormInstance } from 'element-plus'
-import type { Record } from '@/types/record';
-import {record} from '@/http/api'
-const RecordTag = [
-    "无",
-    "穿着",
-    "饮食",
-    "交通",
-    "住宿",
-    "娱乐",
-    "医疗",
-    "缴费",
-    "其他"
-]
-const rules = reactive({
-  date: [
-    { required: true, message: '请选择日期', trigger: 'blur' },
-  ],
-  money: [
-    { required: true, message: '请输入金额', trigger: 'blur' },
-    {validator: (rule: any, value: number, callback: (arg0: Error | undefined) => void) => {
-      if (value === 0) {
-        callback(new Error('金额不能为0'));
-      } else {
-        callback(undefined);
-      }
-    }, trigger: 'blur'},
-    // { type: 'number', message: '请输入数字', trigger: 'blur' },
-    // { min: -100000000, max: 100000000, message: '金额范围为-100000000~100000000', trigger: 'blur' },
-  ],
-  tag: [
-    { required: true, message: '请选择分类', trigger: 'blur' },
-    { validator: (rule: any, value: number, callback: (arg0: Error | undefined) => void) => {
-      if (value === 0) {
-        callback(new Error('分类不能为无'));
-      } else {
-        callback(undefined);
-      }
-    },
-    trigger: 'blur'
-  }
-  ],
-})
-const formatDate = (date: Date) => {
-    return date.toISOString().slice(0, 10).replace(/-/g, '');
-}
-const formatUploadDate = (date: Date) => {
-  if(date){recordForm.date = formatDate(date)}
-}
-const disabledDate = (date: Date) => {
-  return date.getTime()> Date.now()
-}
-const recordFormRef = ref<FormInstance>()
-const recordForm = reactive<Record>({
-  date: '',
-  money: 0,
-  tag:0,
-  remark: '',
-})
-const submitForm = async (formEl: FormInstance | undefined) => {
-  if (!formEl) return
-  await formEl.validate((valid, fields) => {
-    if (valid) {
-      recordForm.money*=100
-      // record(recordForm).then(res => {
-      //   ElMessage.success('新增记录成功')
-      //   console.log(res)
-      //   // recordForm.clear()
-      //   recordFormRef.value?.resetFields()
-      // }).catch(err => {
-      //   console.log(err)
-      // })
-      console.log(recordForm)
-
-    } else {
-      console.log('error submit!', fields)
-    }
-  })
-}
+import {ref} from 'vue'
+import UploadRecordView from './UploadRecordView.vue';
 const defaultTime = ref<[Date, Date]>([
 new Date(new Date().getFullYear(), new Date().getMonth(), 1, 0, 0, 0),
 new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate(), 23, 59, 59),
@@ -179,10 +72,6 @@ const time_range_data = ref(defaultTime.value)
     height: 100%;
     padding: 5px 0;
     text-align: center;
-    .el-form-item {
-      margin-bottom: 10px;
-      width: 100%;
-      
-    }
+    
   }
-</style >: any: number: (arg0: Error | undefined) => void
+</style >
