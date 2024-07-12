7cefeb3de54c5d6942552cc601b4a561bf58d615
Squareroot2-Bit <Squareroot_2@outlook.com>
2024-07-13 00:44:29 +0800

    优化import

--- back-end/PersonalFinance/src/main/java/com/lwjandlyw/personalfinance/service/IERecordService.java
+++ back-end/PersonalFinance/src/main/java/com/lwjandlyw/personalfinance/service/IERecordService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.stereotype.Service;
 
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
